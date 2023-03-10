d40e83503c01e95080883af64bf4a16b0a91e07c
tamar Gogebashvili <tamuna.gogebashvili@gmail.com>
2023-03-10 10:12:29 +0100

    initial commit

---
+++ src/main/java/com/gogebashvili/recsystem/RecsystemApplication.java
@@ -0,0 +1,13 @@
+package com.gogebashvili.recsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gogebashvili/recsystem/RecsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gogebashvili.recsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
